cf22216b73321b1f86ce88d732d167ac5c4fee6f
andrespi97 <andreslage97@gmail.com>
2024-08-12 16:33:50 +0200

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import ToDoList from "./components/todolist";
 
 function App() {
+  const tasks = [
+    {
+      status: "in progress",
+      name: "Task 1",
+      startDate: new Date("2024-08-01"),
+      deadline: new Date("2024-08-10"),
+      duration: 10,
+      listOrigin: "List A",
+    },
+    // Añade más tareas aquí
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 flex items-center justify-center h-screen bg-libreta">
+      <ToDoList tasks={tasks} />
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        libreta: "url('/img/libreta.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
