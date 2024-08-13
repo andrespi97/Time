0228196675584d014bcd4397c69131b7fe65598f
andrespi97 <andreslage97@gmail.com>
2024-08-13 14:47:53 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import ToDoList from "./components/todolist";
+import { useState } from "react";
 
 function App() {
-  const tasks = [
-    {
-      status: "in progress",
-      name: "Task 1",
-      startDate: new Date("2024-08-01"),
-      deadline: new Date("2024-08-10"),
-      duration: 10,
-      listOrigin: "List A",
-    },
-    // Añade más tareas aquí
-  ];
-
-  return (
-    <div className="bg-gray-100 flex items-center justify-center h-screen bg-libreta">
-      <ToDoList tasks={tasks} />
-    </div>
-  );
+  return <div className="bg-libreta"></div>;
 }
 
 export default App;
